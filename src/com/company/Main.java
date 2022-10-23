e6027f58ac41a63d4b7c5779addb5243989b9aa3
ahmadgul <ag2732935@gmail.com>
2022-10-23 10:16:29 +0430

    ArrayList completed

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Java ArrayList class uses a dynamic array
+//        for storing the elements. It is like an array, but there is no size limit.
+
+        ArrayList <String> list=new ArrayList<>();
+        list.add("ahmad");
+        list.add("jan");
+        list.add("someone else");
+//        System.out.println(list);
+
+        //Iterating ArrayList using Iterator
+//        Iterator iterator= list.iterator();
+//
+//        while (iterator.hasNext())
+//        {
+//            System.out.println(iterator.next());
+//        }
+        //Iterating ArrayList using For-each loop
+        /*for (String names:list)
+        {
+            System.out.println(names);
+        }*/
+
+
+//        System.out.println(list.get(2));
+//        list.add(2, "ahmadgul");
+//        System.out.println(list.get(2));
+//        list.isEmpty();
+//        list.indexOf();
+//        list.clear();
+//        list.remove();
+//        list.size();
+//        list.contains();
+//        list.removeAll();
+        //---------------------------------------
+
+    }
+}
